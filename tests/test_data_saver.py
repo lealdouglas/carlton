e54d04bacfbb9f79c385b9cd1b7f2880a8cfa7ac
Leal <douglas.sleal@outlook.com>
2024-11-11 23:58:10 -0300

    aumentando cobertura

--- tests/test_data_saver.py
+++ tests/test_data_saver.py
@@ -1,28 +1,35 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 
+from carlton.ingest.config_ingestor import ConfigIngestor
+from carlton.ingest.config_validator import ConfigValidator
 from carlton.ingest.data_saver import DataSaver
+from carlton.utils.logger import log_error, log_info
 
 
 @pytest.fixture
 def spark():
-    return MagicMock(spec=SparkSession)
+    return SparkSession.builder.master('local').appName('test').getOrCreate()
 
 
 @pytest.fixture
-def df():
-    return MagicMock()
+def df(spark):
+    data = [('Alice', 1), ('Bob', 2)]
+    columns = ['name', 'value']
+    return spark.createDataFrame(data, columns)
 
 
 @pytest.fixture
 def config_ingest():
     return {
-        'schema_name': 'test_schema',
-        'table_name': 'test_table',
+        'schema_name': 'default',
+        'table_name': 'example_table',
         'table_path': '/path/to/table',
         'type_run': 'batch',
+        'trigger_processing_time': '10 seconds',
+        'file_extension': 'parquet',
     }
 
 
@@ -31,106 +38,51 @@ def custom_config_spark():
     return {'spark.some.config.option': 'some-value'}
 
 
-# @patch('carlton.ingest.data_saver.ConfigValidator.validate_args')
-# @patch('carlton.ingest.data_saver.ConfigIngestor.config_ingest_tgt')
-# @patch('carlton.ingest.data_saver.log_info')
-# @patch('carlton.ingest.data_saver.log_error')
-# def test_save_data_success(
-#     mock_log_error,
-#     mock_log_info,
-#     mock_config_ingest_tgt,
-#     mock_validate_args,
-#     spark,
-#     df,
-#     config_ingest,
-#     custom_config_spark,
-# ):
-#     """
-#     Testa a função save_data para verificar se os dados são salvos corretamente.
-#     Tests the save_data function to check if data is saved correctly.
-#     """
-#     # Mock the Spark session and DataFrame
-#     mock_spark = spark
-#     mock_df = df
-
-#     # Mock the config_ingest_tgt function
-#     mock_config_ingest_tgt.return_value = {
-#         'checkpointLocation': '/path/to/checkpoint',
-#         'path': '/path/to/table',
-#         'mergeSchema': True,
-#     }
-
-#     # Call the save_data function
-#     DataSaver.save_data(
-#         mock_spark, mock_df, config_ingest, custom_config_spark
-#     )
-
-#     # Assertions
-#     mock_validate_args.assert_any_call(
-#         ['schema_name', 'table_name', 'table_path', 'type_run'], config_ingest
-#     )
-#     mock_config_ingest_tgt.assert_called_once_with(
-#         config_ingest, custom_config_spark
-#     )
-#     mock_log_info.assert_any_call(
-#         f'Configurations used for writing: {mock_config_ingest_tgt.return_value}'
-#     )
-
-#     # Verify table creation query
-#     expected_query = f"""CREATE TABLE IF NOT EXISTS {config_ingest['schema_name']}.{config_ingest['table_name']} (,_rescued STRING,carlton_current_date DATE,carlton_metadata struct<file_path:string,file_name:string,file_size:bigint,file_block_start:bigint,file_block_length:bigint,file_modification_time:timestamp>) USING DELTA CLUSTER BY (carlton_current_date)"""
-#     mock_spark.sql.assert_called_once_with(expected_query)
-
-#     # Verify writeStream call
-#     mock_df.writeStream.options.assert_called_once_with(
-#         **mock_config_ingest_tgt.return_value
-#     )
-#     mock_df.writeStream.options.return_value.outputMode.assert_called_once_with(
-#         'append'
-#     )
-#     mock_df.writeStream.options.return_value.outputMode.return_value.trigger.assert_called_once_with(
-#         availableNow=True
-#     )
-#     mock_df.writeStream.options.return_value.outputMode.return_value.trigger.return_value.table.assert_called_once_with(
-#         f"{config_ingest['schema_name']}.{config_ingest['table_name']}"
-#     )
-#     mock_df.writeStream.options.return_value.outputMode.return_value.trigger.return_value.table.return_value.awaitTermination.assert_called_once()
-
-#     # Ensure log_error is not called
-#     mock_log_error.assert_not_called()
-
-
-@patch('carlton.ingest.data_saver.ConfigValidator.validate_args')
-@patch('carlton.ingest.data_saver.ConfigIngestor.config_ingest_tgt')
+@patch('carlton.ingest.data_saver.ConfigValidator')
+@patch('carlton.ingest.data_saver.ConfigIngestor')
 @patch('carlton.ingest.data_saver.log_info')
 @patch('carlton.ingest.data_saver.log_error')
-def test_save_data_exception(
+@patch('pyspark.sql.DataFrame.writeStream', new_callable=MagicMock)
+def test_save_data(
+    mock_write_stream,
     mock_log_error,
     mock_log_info,
-    mock_config_ingest_tgt,
-    mock_validate_args,
+    mock_config_ingestor,
+    mock_config_validator,
     spark,
     df,
     config_ingest,
     custom_config_spark,
 ):
-    """
-    Testa a função save_data para verificar se os erros são logados corretamente.
-    Tests the save_data function to check if errors are logged correctly.
-    """
-    # Mock the Spark session and DataFrame
-    mock_spark = spark
-    mock_df = df
-
-    # Mock the config_ingest_tgt function to raise an exception
-    mock_config_ingest_tgt.side_effect = Exception('Test exception')
-
-    # Call the save_data function
-    DataSaver.save_data(
-        mock_spark, mock_df, config_ingest, custom_config_spark
+    # Mock the methods
+    mock_config_validator.validate_args = MagicMock()
+    mock_config_ingestor.config_ingest_tgt = MagicMock(
+        return_value={'path': '/path/to/save'}
     )
 
-    # Check if log_error is called with the exception message
-    mock_log_error.assert_called_once_with('Test exception')
+    # Mock the writeStream method chain
+    mock_write_stream_instance = mock_write_stream.return_value
 
-    # Ensure log_info is not called
-    mock_log_info.assert_not_called()
+    # Call the method
+    DataSaver.save_data(spark, df, config_ingest, custom_config_spark)
+
+    # Assertions
+    mock_config_validator.validate_args.assert_any_call(
+        ['schema_name', 'table_name', 'table_path', 'type_run'],
+        config_ingest,
+    )
+    mock_config_ingestor.config_ingest_tgt.assert_called_once_with(
+        config_ingest, custom_config_spark
+    )
+    mock_log_info.assert_any_call(
+        f"Configurations used for writing: {{'path': '/path/to/save'}}"
+    )
+    mock_log_info.assert_any_call(
+        f"Registering table {config_ingest['table_name']} in schema {config_ingest['schema_name']}"
+    )
+    mock_log_info.assert_any_call(
+        f'Using liquid_cluster. Managed by the column carlton_current_date'
+    )
+    mock_log_info.assert_any_call(
+        f'Registering table: CREATE TABLE IF NOT EXISTS {config_ingest["schema_name"]}.{config_ingest["table_name"]} (name STRING, value STRING,_rescued STRING,carlton_current_date DATE,carlton_metadata struct<file_path:string,file_name:string,file_size:bigint,file_block_start:bigint,file_block_length:bigint,file_modification_time:timestamp>) USING DELTA CLUSTER BY (carlton_current_date)'
+    )
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -16,60 +16,51 @@ from carlton.mock.generate import (
     send_event_to_eventhub,
 )
 
-# Valores para gerar ações de usuários
-TIPOS_ACAO = [
-    'login',
-    'consulta_saldo',
-    'transferencia',
-    'pagamento',
-    'logout',
-]
-DISPOSITIVOS = ['Android', 'iOS']
-
 
 @pytest.fixture
-def mock_credential():
-    """
-    Fixture para mockar ClientSecretCredential.
-    """
-    with patch('carlton.mock.generate.ClientSecretCredential') as mock:
-        yield mock
+def root_properties():
+    return {
+        'event_hub_namespace': 'test_namespace',
+        'event_hub_name': 'test_eventhub',
+        'sleep_time': 5,
+    }
 
 
-@pytest.fixture
-def mock_producer_client():
-    """
-    Fixture para mockar EventHubProducerClient.
-    """
-    with patch('carlton.mock.generate.EventHubProducerClient') as mock:
-        yield mock
+@patch('carlton.mock.generate.ClientSecretCredential')
+@patch('carlton.mock.generate.EventHubProducerClient')
+@patch('carlton.mock.generate.log_info')
+@patch('carlton.mock.generate.log_error')
+@patch('carlton.mock.generate.send_event_to_eventhub')
+def test_generate_mock_data(
+    mock_send_event,
+    mock_log_error,
+    mock_log_info,
+    mock_producer_client,
+    mock_credential,
+    root_properties,
+):
+    # Mock the methods
+    mock_credential.return_value = MagicMock()
+    mock_producer = mock_producer_client.return_value
 
+    # Call the method
+    generate_mock_data(root_properties)
 
-@pytest.fixture
-def mock_send_event():
-    """
-    Fixture para mockar send_event_to_eventhub.
-    """
-    with patch('carlton.mock.generate.send_event_to_eventhub') as mock:
-        yield mock
-
-
-# def test_send_event_to_eventhub(mock_producer_client):
-#     """
-#     Testa a função send_event_to_eventhub para verificar se os eventos são enviados corretamente.
-#     """
-#     mock_producer = mock_producer_client.return_value
-#     root_properties = {
-#         'event_hub_namespace': 'test_namespace',
-#         'event_hub_name': 'test_eventhub',
-#     }
-#     credential = MagicMock()
-#     event_data = {'key': 'value'}
-
-#     send_event_to_eventhub(root_properties, credential, event_data)
-
-#     assert mock_producer.create_batch.called
-#     assert mock_producer.send_batch.called
+    # Assertions
+    mock_credential.assert_called_once_with(
+        tenant_id='ARM_TENANT_ID',
+        client_id='ARM_CLIENT_ID',
+        client_secret='ARM_CLIENT_SECRET',
+    )
+    mock_producer_client.assert_called_once_with(
+        fully_qualified_namespace='test_namespace.servicebus.windows.net',
+        eventhub_name='test_eventhub',
+        credential=mock_credential.return_value,
+    )
+    mock_log_info.assert_any_call('test_namespace.servicebus.windows.net')
+    mock_log_info.assert_any_call('test_eventhub')
+    mock_send_event.assert_called_once()
+    mock_log_error.assert_not_called()
 
 
 def test_generate_timestamps_real_time():
@@ -83,48 +74,40 @@ def test_generate_timestamps_real_time():
         assert (timestamps[i] - timestamps[i - 1]).seconds == 5
 
 
-# def test_gerar_dados_consentimentos():
-#     """
-#     Testa a função gerar_dados_consentimentos para verificar se os consentimentos são gerados corretamente.
-#     """
-#     num_records = 5
-#     user_ids = range(10000, 10005)
-#     consentimentos = gerar_dados_consentimentos(num_records, user_ids)
-#     assert len(consentimentos) == num_records
-#     for consentimento in consentimentos:
-#         assert consentimento['user_id'] in user_ids
-#         assert consentimento['status'] in ['Ativo', 'Revogado']
-#         assert 'consent_id' in consentimento
-#         assert 'data_inicio' in consentimento
-#         assert 'data_fim' in consentimento
-#         assert 'tipo_dados' in consentimento
-#         assert 'plataforma_origem' in consentimento
+def test_gerar_dados_consentimentos():
+    """
+    Testa a função gerar_dados_consentimentos para verificar se os consentimentos são gerados corretamente.
+    """
+    consentimentos = gerar_dados_consentimentos()
+    assert len(consentimentos) == 21  # 10000 to 10020 inclusive
+    for consentimento in consentimentos:
+        assert 'consent_id' in consentimento
+        assert 'user_id' in consentimento
+        assert 'data_inicio' in consentimento
+        assert 'data_fim' in consentimento
+        assert 'tipo_dados' in consentimento
+        assert 'tipo_id' in consentimento
+        assert 'status' in consentimento
+        assert 'plataforma_origem' in consentimento
 
 
-# def test_generate_mock_data(
-#     mock_send_event, mock_producer_client, mock_credential
-# ):
-#     """
-#     Testa a função generate_mock_data para verificar se o fluxo principal é executado corretamente.
-#     """
-#     mock_producer = mock_producer_client.return_value
-#     mock_credential.return_value = MagicMock()
+@patch('carlton.mock.generate.EventHubProducerClient')
+def test_send_event_to_eventhub(mock_producer_client):
+    """
+    Testa a função send_event_to_eventhub para verificar se os eventos são enviados corretamente.
+    """
+    mock_producer = mock_producer_client.return_value
+    root_properties = {
+        'event_hub_namespace': 'test_namespace',
+        'event_hub_name': 'test_eventhub',
+    }
+    credential = MagicMock()
+    event_data = {'key': 'value'}
 
-#     root_properties = {
-#         'event_hub_namespace': 'test_namespace',
-#         'event_hub_name': 'test_eventhub',
-#     }
+    send_event_to_eventhub(
+        root_properties, mock_producer, credential, event_data
+    )
 
-#     generate_mock_data(root_properties, num_users=1, sleep_time=0)
-
-#     mock_credential.assert_called_once_with(
-#         tenant_id='ARM_TENANT_ID',
-#         client_id='ARM_CLIENT_ID',
-#         client_secret='ARM_CLIENT_SECRET',
-#     )
-#     mock_producer_client.assert_called_once_with(
-#         fully_qualified_namespace='test_namespace.servicebus.windows.net',
-#         eventhub_name='test_eventhub',
-#         credential=mock_credential.return_value,
-#     )
-#     assert mock_send_event.called
+    mock_producer.create_batch.assert_called_once()
+    mock_producer.send_batch.assert_called_once()
+    mock_producer.close.assert_called_once()
